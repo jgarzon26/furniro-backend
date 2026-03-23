9982c9f3cabb1e44f3df794f5a937e7a33bc3d02
jgarzon26 <janusseangarzon@gmail.com>
2026-03-23 14:06:13 +0800

    fix importing issues

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { MongoModule } from 'src/databases/mongo.module.js';
+import { MongoModule } from '../databases/mongo.module.js';
 import { User, UserSchema } from './user.schema.js';
 import { UserResolver } from './user.resolver.js';
 import { UserService } from './user.service.js';
